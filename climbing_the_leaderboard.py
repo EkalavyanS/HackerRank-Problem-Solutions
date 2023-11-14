cf583f5d664a5c776114366a9fab5ba4ad20909f
EkalavyanS <101187280+EkalavyanS@users.noreply.github.com>
2023-11-14 16:00:29 +0530

    Create climbing_the_leaderboard.py

---
+++ climbing_the_leaderboard.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'climbingLeaderboard' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY ranked
+#  2. INTEGER_ARRAY player
+#
+
+def climbingLeaderboard(ranked, player):
+    unique_ranked = sorted(list(set(ranked)), reverse=True)
+    player_rank = []
+
+    for score in player:
+        while unique_ranked and score >= unique_ranked[-1]:
+            unique_ranked.pop()
+        player_rank.append(len(unique_ranked) + 1)
+
+    return player_rank
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ranked_count = int(input().strip())
+
+    ranked = list(map(int, input().rstrip().split()))
+
+    player_count = int(input().strip())
+
+    player = list(map(int, input().rstrip().split()))
+
+    result = climbingLeaderboard(ranked, player)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
